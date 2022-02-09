a1af409ac3b89d3610a67b4a08b3b09068ab92c5
Jan Polách <polachj.05@spst.cz>
2022-02-09 08:41:26 +0100

    nasobeni + deleni

--- kok/Form1.cs
+++ kok/Form1.cs
@@ -20,9 +20,11 @@ namespace kok
         private void button1_Click(object sender, EventArgs e)
         {
             int a = Convert.ToInt32(textBox1.Text);
-            int b = Convert.ToInt32(textBox2.Text);
+            double b = Convert.ToInt32(textBox2.Text);
             label1.Text = Convert.ToString(a + b);
             label2.Text = Convert.ToString(a - b);
+            label3.Text = Convert.ToString(a * b);
+            label4.Text = (a / b).ToString("F3");
         }
     }
 }
